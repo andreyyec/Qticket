727562c0b2139bef5313b0e0e9e1a08d849d40cb
andreyc <andreyyec@gmail.com>
2017-10-11 21:41:42 -0600

    Added user interaction to apply values to products

--- application/public/static/js/io-handler.js
+++ application/public/static/js/io-handler.js
@@ -1,8 +1,8 @@
 $(function () {
-    let self, sckId, csOrdersArray, currentRow,
+    let self, sckId, csOrdersArray, currentRow = {}, override = true, actionEnabled = false,
     socket = io(),
     body = $('body'),
-    disableClass = 'disabled',
+    disabledClass = 'disabled',
     selectedClass = 'selected',
     ordersContainer = body.find('.orders-screen'),
     orderDetailsContainer = body.find('.order-details-screen'),
@@ -13,6 +13,10 @@ $(function () {
     oDProductsHolder = oDSection.find('.order-products-holder'),
     oDKeypad = oDSection.find('.order-keypad'),
     modifierButtons = oDKeypad.find('.modifier'),
+    keyButtons = oDKeypad.find('.key'),
+    removeModifier = $(modifierButtons[0]),
+    priceModifier = $(modifierButtons[1]),
+    qtyModifier = $(modifierButtons[2]),
     templates= {
         orderCard: '<div class="col-xs-6 col-sm-4 col-md-3 order-card" orderid="${id}">\
                         <div class="panel panel-primary">\
@@ -59,45 +63,212 @@ $(function () {
         row:    '<div class="order-row" data-id="${id}">\
                     <div class="row inner" >\
                         <div class="col-8 bold product-name">${product}</div>\
-                        <div class="col-4 product-price">&cent;${price}</div>\
+                        <div class="col-4 product-price">&cent;<span class="price">${price}</span></div>\
                         <div class="col-12 product-qty">Cantidad: <span class="bold qty">1</span></div>\
                     </div>\
                 </div>'
     },
     uiManager = {
+        resetActionButtons: function () {
+            removeModifier.removeClass(disabledClass);
+            priceModifier.addClass(disabledClass);
+            qtyModifier.removeClass(disabledClass);
+        },
         enableActionButtons: function() {
-            modifierButtons.removeClass(disableClass);
+            actionEnabled = true;
+            override = true;
+            uiManager.resetActionButtons();
         },
         disableActionButtons: function() {
-            modifierButtons.addClass(disableClass);
+            modifierButtons.addClass(disabledClass);
+            writeQueue = '';
+            actionEnabled = false;
+        },
+        getActiveAction: function() {
+            return (!priceModifier.hasClass(disabledClass)) ? 'price' : 'qty';
         },
         cleanDetailUI: function() {
             productCards.removeClass(selectedClass);
+            uiManager.disableActionButtons;
         },
         addRow: function(card) {
-            let nRowInfo = {id: card.attr('data-id'),
+            if (currentRow.element === undefined || uiManager.currentRowValid()) {
+                let nRowInfo = {id: card.attr('data-id'),
                         product: card.attr('data-name'),
                         price: card.attr('data-price')},
                 nRow = $.tmpl(templates.row, nRowInfo).appendTo(oDProductsList);
-            uiManager.updateCurrentRow(nRow);
-            uiManager.enableActionButtons();
-            card.addClass(selectedClass);
+                oDProductsList.scrollTop(oDProductsList.prop('scrollHeight'));
+                uiManager.updateCurrentRow(nRow);
+                card.addClass(selectedClass);
+            }
         },
-        removeRow: function(row) {
+        removeRow: function() {
+            productsSection.find('.product-card[data-id='+currentRow.element.attr('data-id')+']').removeClass(selectedClass);
+            currentRow.element.remove();
+            currentRow = {};
+            uiManager.disableActionButtons();
+        },
+        validValue: function(value, field) {
+            if(value !== 0) {
+                return true;
+            } else {
+                new Noty({
+                    type: 'error',
+                    layout: 'topRight',
+                    timeout: 2000,
+                    text: 'Error en ' + field
+                }).show();
+                return false;
+            }
+        },
+        currentRowValid: function() {
+            let floatPrice = parseFloat(currentRow.price.html()),
+                floatQty = parseFloat(currentRow.qty.html());
 
+                currentRow.price.html(floatPrice);
+                currentRow.qty.html(floatQty);
+
+            return (uiManager.validValue(floatPrice, 'precio') && uiManager.validValue(floatQty, 'cantidad')) ? true : false;
         },
         updateCurrentRow: function (row) {
-            if (currentRow !== undefined) {
-                currentRow.removeClass(selectedClass);
-                row.addClass(selectedClass);
-                currentRow = row;
+            if (currentRow.element !== undefined) {
+                 if (uiManager.currentRowValid()) {
+                    currentRow.element.removeClass(selectedClass);
+                    row.addClass(selectedClass);
+                    currentRow.element = row;
+                    currentRow.price = row.find('.price');
+                    currentRow.qty = row.find('.qty');
+                    uiManager.enableActionButtons();
+                }
             } else {
                 row.addClass(selectedClass);
-                currentRow = row;
+                currentRow.element = row;
+                currentRow.price = row.find('.price');
+                currentRow.qty = row.find('.qty');
+                uiManager.enableActionButtons();
             }
         },
         attachListeners: function() {
-            productsSection.on('click', '.product-card', function(e){
+            removeModifier.on('click', function(e) {
+                if (actionEnabled) {
+                    let currentVal;
+                    if (uiManager.getActiveAction() === 'price') {
+                        currentVal = currentRow.price.html();
+
+                        if (currentVal !== '0') {
+                            if (!override) {
+                                if (currentRow.price.html().length === 1) {
+                                    currentRow.price.html(0);
+                                } else {
+                                    currentRow.price.html(currentVal.slice(0, -1));
+                                }
+                            }else {
+                                currentRow.price.html(0);
+                            }
+                        } else {
+                            uiManager.removeRow(currentRow.element);
+                        }
+                    } else {
+                        currentVal = currentRow.qty.html();
+
+                        if (currentVal !== '0') {
+                            if (!override) {
+                                if (currentRow.qty.html().length === 1) {
+                                    currentRow.qty.html(0);
+                                } else {
+                                    currentRow.qty.html(currentVal.slice(0, -1));
+                                }
+                            }else {
+                                currentRow.qty.html(0);
+                            }
+                        } else {
+                            uiManager.removeRow(currentRow.element);
+                        }
+                    }
+                }
+            });
+
+            priceModifier.on('click', function(e) {
+                if (actionEnabled) {
+                    override = true;
+                    if (uiManager.getActiveAction() === 'qty') {
+                        qtyModifier.addClass(disabledClass);
+                        priceModifier.removeClass(disabledClass);
+                    }
+                }
+            });
+
+            qtyModifier.on('click', function(e) {
+                if (actionEnabled) {
+                    override = true;
+                    if (uiManager.getActiveAction() === 'price') {
+                        qtyModifier.removeClass(disabledClass);
+                        priceModifier.addClass(disabledClass);
+                    }
+                }
+            });
+
+            keyButtons.on('click', function(e) {
+                let target = $(e.currentTarget);
+                //@TODO: Create single method to validate and mask inputs
+
+                if (override) {
+                    if (uiManager.getActiveAction() === 'price') {
+                        if (target.html() === '.') {
+                            currentRow.price.html('0.');
+                        } else {
+                            currentRow.price.html(target.html());
+                        }
+                    } else {
+                        if (target.html() === '.') {
+                            currentRow.qty.html('0.');
+                        } else {
+                            currentRow.qty.html(target.html());
+                        }
+                    }
+                    override = false;
+                } else {
+                    if (uiManager.getActiveAction() === 'price') {
+                        let currentVal = currentRow.price.html();
+
+                        if (currentVal !== '0') {
+                            if ((target.html() === '.' && !currentVal.includes('.')) || target.html() !== '.') {
+                                if (currentVal.length <= 6) {
+                                    currentRow.price.html(currentVal + target.html());
+                                }
+                            }
+                        } else {
+                            if (target.html() === '.') {
+                                currentRow.price.html(currentVal + target.html());
+                            } else {
+                                currentRow.price.html(target.html());
+                            }
+                        }
+                    } else {
+                       let currentVal = currentRow.qty.html();
+
+                        if (currentVal !== '0') {
+                            if ((target.html() === '.' && !currentVal.includes('.')) || target.html() !== '.') {
+                                if (currentVal.length <= 6) {
+                                    currentRow.qty.html(currentVal + target.html());
+                                }
+                            }
+                        } else {
+                            if (target.html() === '.') {
+                                currentRow.qty.html(currentVal + target.html());
+                            } else {
+                                currentRow.qty.html(target.html());
+                            }
+                        }
+                    }
+                }
+            });
+
+            oDProductsList.on('click', '.order-row', function(e) {
+                uiManager.updateCurrentRow($(e.currentTarget));
+            });
+
+            productCards.on('click', function(e) {
                 let card = $(e.currentTarget);
                 if (!card.hasClass(selectedClass)) {
                     uiManager.addRow(card);
