942834949c91765d00e2ed52c32f3fa51889486d
Andrey Castro Carvajal <andreyyec@gmail.com>
2017-10-20 10:25:30 -0600

    Updated Orders Request Process

--- application/models/OrdersManager.js
+++ application/models/OrdersManager.js
@@ -4,7 +4,7 @@ const   constants = require('../config/constants'),
         http = require('http'),
         request = require('request');
 
-let self, io, cookie, ordersArray = [], sckId;
+let self, io, cookie, sckId, ordersObj = {'drafts': [], 'confirmed': [], 'approved': []};
 
 class OrdersManager {
 
@@ -18,7 +18,7 @@ class OrdersManager {
     attachIOListeners() {
         //Web Socket Settings
         io.on('connection', function(socket){
-            socket.emit('init', {sID: sckId, data: ordersArray});
+            socket.emit('init', {sID: sckId, data: ordersObj.drafts});
         });
 
         io.on('blockOrder', (orderID) => {
@@ -43,28 +43,35 @@ class OrdersManager {
         return {sID: sckId, data: data};
     }
 
-    getIdslist() {
-        let idsArray = [];
-        for (let key in ordersArray) {
-            idsArray.push({id:ordersArray[key].id, last_update: ordersArray[key].last_update});    
+    getIdsObj() {
+        let section, idsObj = {};
+
+        for (let sectInd in ordersObj) {
+            section = ordersObj[sectInd];
+            idsObj[sectInd] = new Array();
+
+            for (let doc in section) {
+                idsObj[sectInd].push({id:section[doc].id, last_update:section[doc].last_update});    
+            }
         }
-        return idsArray;
+        return idsObj;
     }
 
-    updateAppPurchasesList(changesList) {
+    updateList(changesList, checkForUpdated = false) {
+        let updatesArray = [], updateFlag = false;
+
         if (changesList) {
-            let updateFlag = false;
             if (changesList.added && changesList.added.length > 0) {    
                 for (let key in changesList.added) {
-                    ordersArray.push(changesList.added[key])
+                    updatesArray.push(changesList.added[key])
                 }
                 updateFlag = true;
             }
-            if (changesList.updated && changesList.updated.length > 0) {
+            if (checkForUpdated && changesList.updated && changesList.updated.length > 0) {
                 for (let xKey in changesList.updated) {
-                    for (let yKey in ordersArray) {
-                        if (changesList.updated[xKey].id === ordersArray[yKey].id) {
-                            ordersArray[yKey] = changesList.updated[xKey]
+                    for (let yKey in updatesArray) {
+                        if (changesList.updated[xKey].id === updatesArray[yKey].id) {
+                            updatesArray[yKey] = changesList.updated[xKey]
                         }
                     }
                 }
@@ -72,32 +79,59 @@ class OrdersManager {
             }
             if (changesList.removed && changesList.removed.length > 0) {
                 for (let xKey in changesList.removed) {
-                    for (let yKey in ordersArray) {
-                        if (changesList.removed[xKey] === ordersArray[yKey].id) {
-                            let index = ordersArray.indexOf(ordersArray[yKey]);
+                    for (let yKey in updatesArray) {
+                        if (changesList.removed[xKey] === updatesArray[yKey].id) {
+                            let index = updatesArray.indexOf(updatesArray[yKey]);
                             if (index > -1) {
-                                ordersArray.splice(index, 1);
+                                updatesArray.splice(index, 1);
                             }
 
-                            delete ordersArray[yKey];
+                            delete updatesArray[yKey];
                         }
                     }
                 }
                 updateFlag = true;
             }
+        }
+        return {updated: updateFlag, updatedObject: updatesArray};
+    }
 
-            if (updateFlag) {
-                io.emit('ordersUpdate', self.getSocketMessage(changesList));
-            }    
-        } else {
-            console.log('Odoo data wasn\'t received');
-        }        
+    updateAppInMemoryList(changesList) {
+        let updtbject, updateEvFlag = false;
+
+        if (changesList.drafts.added.length > 0 || changesList.drafts.updated.length > 0 || changesList.drafts.removed.length > 0) {
+            updtbject = self.updateList(changesList.drafts, true);
+            if (updtbject.updated === true) {
+                updateEvFlag = true;
+                ordersObj.drafts = updtbject.updatedObject;
+            }
+        }
+
+        if (changesList.confirmed.added.length > 0 || changesList.confirmed.removed.length > 0) {
+            updtbject = self.updateList(changesList.confirmed, true);
+            if (updtbject.updated === true) {
+                updateEvFlag = true;
+                ordersObj.confirmed = updtbject.updatedObject;
+            }
+        }
+
+        if (changesList.approved.added.length > 0 || changesList.approved.removed.length > 0) {
+            updtbject = self.updateList(changesList.approved, true);
+            if (updtbject.updated === true) {
+                updateEvFlag = true;
+                ordersObj.approved = updtbject.updatedObject;
+            }
+        }
+
+        /*if (updateEventInitFlag) {
+            io.emit('ordersUpdate', self.getSocketMessage(changesList));
+        }*/
     }
 
     requestOrderList() {
         return new Promise((resolve, reject) => {
             let restServPath = '/rest/purchases/drafts/list',
-                idsArray = self.getIdslist(),
+                idsObj = self.getIdsObj(),
                 opts = {
                     url : odooSettings.protocol+'://'+odooSettings.host+':'+odooSettings.port + restServPath,
                     method: 'post',
@@ -107,15 +141,13 @@ class OrdersManager {
                     },
                     body: {
 					    params: {
-					        ids: {
-					        	ids: idsArray
-					        },
+					        ids:  idsObj
 					    }
                     }
                 };
-
             request(opts, function (error, response, body) {
                 if (error === null) {
+                    console.log(body.result);
                     resolve(body.result);
                 } else {
                     reject();
@@ -128,12 +160,11 @@ class OrdersManager {
         let getDraftsList = self.requestOrderList();
 
         getDraftsList.then((data) => {
-            console.log('Data:');
-            console.log(data);
-            self.updateAppPurchasesList(JSON.parse(data));
+            self.updateAppInMemoryList(JSON.parse(data));
         })
         .catch((data) => {
-             console.log('Error while trying to get purchases list');
+            console.log(data);
+            console.log('Error while trying to get purchases list');
         }); 
     }
 
